fbba765bf236df4bbf695278e0d00b9c187b84df
hfDev <dev.faramarzi369@gmail.com>
2023-09-24 20:05:53 +0330

    update todo status

--- src/pages/Todos/Index.js
+++ src/pages/Todos/Index.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react"
-
 import axios from "axios"
 import Swal from "sweetalert2"
-
 
 const IndexTodods = () => {
     const [todos, setTodos] = useState(null)
@@ -54,7 +52,7 @@ const IndexTodods = () => {
                     setLoading(false)
                     setTodos(res.data)
                     setError(null)
-                    console.log(typeof todos)
+
 
                 })
                 .catch(err => {
@@ -72,14 +70,14 @@ const IndexTodods = () => {
         if (task) {
 
             async function sendData() {
-                await axios.post(`https://jsonplaceholder.typicode.com/todos`, { title: task, completed: false ,userId:1})
+                await axios.post(`https://jsonplaceholder.typicode.com/todos`, { title: task, completed: false, userId: 1 })
                     .then(res => {
                         setLoading(false)
 
-                        console.log('response', res.data)
-                        const newToto=res.data
-                        setTodos([newToto ,...todos])
-                        
+                        // console.log('response', res.data)
+                        const newToto = res.data
+                        setTodos([newToto, ...todos])
+
                         Swal.fire({
                             title: "Task added",
                             icon: "success",
@@ -102,36 +100,49 @@ const IndexTodods = () => {
                     })
             }
             sendData()
-            // =--=
-            // async function fetchData() {
-            //     await axios.get("https://jsonplaceholder.typicode.com/todos")
-            //         .then(res => {
-            //             setLoading(false)
-            //             setTodos(res.data)
-            //             setTodosLen(res.data.length)
-            //             setError(null)
-                     
-            //         })
-            //         .catch(err => {
-            //             setLoading(false)
-    
-            //             setError(err.message)
-            //         })
-            // }
-            // fetchData()
-            
-
         }
         else {
             setLoading(false)
-
         }
+    }
+
+    const changeTodoStatus = (todoId, todoCompleted) => {
+        setLoading(true)
+
+        async function todoUpdate(id,completed) {
+            await axios.put(`https://jsonplaceholder.typicode.com/todos/${id}`, { id: id, completed: !completed })
+                .then(res => {
+                    console.log(res.data);
+                    setLoading(false)
+                    setTodos(todos.map(todo=>(
+                        todo.id === id ? {...todo,completed:!completed} : todo
+                    )))
+                    Swal.fire({
+                        title: "Task updated ...",
+                        icon: "success",
+                        showConfirmButton: false,
+                        timerProgressBar: true,
+                        timer: 3000,
+                        toast: true,
+                        position: 'top',
+                    });
+                })
+                .catch(err => {
+                    setLoading(false)
+                    setError(err.message)
+                    Swal.fire(
+                        'Uooops!',
+                        err.message,
+                        'warning'
+                    )
+                })
+        }
+        todoUpdate(todoId,todoCompleted)
     }
 
     return (
         <>
             <h3 style={{ marginTop: '11px' }}><i className="bi bi-check2-circle"></i> <span> Todos : <span style={{ fontSize: '18px', color: 'green' }}>{todosLen} tasks</span> </span></h3>
-
 
             <form onSubmit={(e) => handleSubmit(e)}>
                 <div className="row mb-3">
@@ -178,14 +189,17 @@ const IndexTodods = () => {
                 todos && todos.map(todo => (
 
                     <div className="col-md-4 mb-3" key={todo.id}>
-                        
+
                         <div className={"card " + (todo.completed && "bg-success")} >
                             <div className="card-body d-flex justify-content-between align-items-center">
                                 <div>
                                     {todo.completed ? <del> {todo.title.substring(0, 33)}</del> : <span> {todo.title.substring(0, 33)}</span>}
                                 </div>
                                 <div className="todo-icons">
-                                    {todo.completed ? <i className="bi bi-check2-all"></i> : <i className="bi bi-check2"></i>}
+                                    {todo.completed ?
+                                        <i className="bi bi-check2-all" onClick={(e) => changeTodoStatus(todo.id,todo.completed)}></i> :
+                                        <i className="bi bi-check2" onClick={(e) => changeTodoStatus(todo.id, todo.completed)} ></i>
+                                    }
 
 
                                     <i className="bi bi-trash-fill"></i>
